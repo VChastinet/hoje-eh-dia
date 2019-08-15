bd02582bb2bd33708f465f2d18f01978782a4833
Victor Chastinet <victorchastinet@live.com>
2019-08-15 00:04:31 -0300

    adding alert modal and build files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import {
   Button,
@@ -7,6 +7,8 @@ import {
   Content,
   Form,
   Heading,
+  Modal,
+  Section
 } from 'react-bulma-components/full';
 
 import mad from './assets/bolsomad.jpg';
@@ -31,22 +33,33 @@ function App() {
   const [answer, setAnswer] = useState('...');
   const [lastDate, setLastDate] = useState('');
   const [loading, setLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleAlert = () => setShowAlert(!showAlert);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    const [year, month, day] = lastDate.split('-');
+    const brDate = new Date(year, month - 1, day);
+
+    if (brDate > CURRENT_DATE) {
+      handleAlert();
+      return;
+    }
+
     setLoading(true);
     setAnswer('think');
 
     setTimeout(() => {
-      const [year, month, day] = lastDate.split('-');
-      const brDate = new Date(year, month - 1, day);
   
       const deadLine = CURRENT_DATE - brDate;
+
   
       const answer = deadLine >= 24 * Math.pow(10, 7) ? 'Sim' : 'Não';
       setAnswer(answer);
       setLoading(false);
-    }, 1000);
+    }, 800);
 
   };
 
@@ -55,42 +68,51 @@ function App() {
   };
 
   return (
-    <Container>
-      <Content>
-        <form onSubmit={handleSubmit}>
-          <Form.Field>
-            <Form.Label style={textColor} size="large">
-              Qual foi a ultima vez que você fez cocô?
-            </Form.Label>
-            <Form.Control size="large" htmlFor="lastDate">
-              <Form.Input
-                size="large"
-                value={lastDate}
-                id="lastDate"
-                onChange={handleChange}
-                type="date"
-              />
-            </Form.Control>
-          </Form.Field>
-          <div className="image">
-            <Image
-              alt="bolso icon"
-              src={answer === '...' ? think : mood[answer]}
-              />
-          </div>
-          <Heading size={1} style={textColor}>{loading ? '...' : answer}</Heading>
-          <Button
-            loading={loading}
-            rounded
-            size="large"
-            color="danger"
-            type="submit"
-          >
-            Hoje é dia de fazer cocô?
-          </Button>
-        </form>
-      </Content>
-    </Container>
+    <div className="main">
+      <Container>
+        <Content>
+          <form onSubmit={handleSubmit}>
+            <Form.Field>
+              <Form.Label style={textColor} size="large">
+                Qual foi a ultima vez que você fez cocô?
+              </Form.Label>
+              <Form.Control size="large" htmlFor="lastDate">
+                <Form.Input
+                  size="large"
+                  value={lastDate}
+                  id="lastDate"
+                  onChange={handleChange}
+                  type="date"
+                />
+              </Form.Control>
+            </Form.Field>
+            <div className="image">
+              <Image
+                alt="bolso icon"
+                src={answer === '...' ? think : mood[answer]}
+                />
+            </div>
+            <Heading size={1} style={textColor}>{loading ? '...' : answer}</Heading>
+            <Button
+              loading={loading}
+              rounded
+              size="large"
+              color="danger"
+              type="submit"
+            >
+              Hoje é dia de fazer cocô?
+            </Button>
+          </form>
+        </Content>
+      </Container>
+      <Modal show={showAlert} onClose={handleAlert}>
+        <Modal.Content>
+          <Section style={{ backgroundColor: '#f8ffff' }}>
+            <p> Você não pode fazer cocô no futuro! </p>
+          </Section>
+        </Modal.Content>
+      </Modal>
+    </div>
   );
 }
 
