80385c89d3d6607cc16cb2747e71d29a1813b7a9
Victor Chastinet <victorchastinet@live.com>
2019-08-15 00:32:23 -0300

    Updates

---
+++ precache-manifest.1e00bbdbf41bb141245368e7896dbda0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb1f314abdc93ea323c5a2f921920e0",
+    "url": "/hoje-eh-dia/index.html"
+  },
+  {
+    "revision": "c1a3879f5635cf79a869",
+    "url": "/hoje-eh-dia/static/css/main.23a41e0f.chunk.css"
+  },
+  {
+    "revision": "415ab3077d3d1b7468cc",
+    "url": "/hoje-eh-dia/static/js/2.03a84cec.chunk.js"
+  },
+  {
+    "revision": "c1a3879f5635cf79a869",
+    "url": "/hoje-eh-dia/static/js/main.6ce0cad1.chunk.js"
+  },
+  {
+    "revision": "a070a081b21d35e50438",
+    "url": "/hoje-eh-dia/static/js/runtime~main.69424e28.js"
+  },
+  {
+    "revision": "3585ef45d3c12d2950ba18df4dcd4bc5",
+    "url": "/hoje-eh-dia/static/media/bolsohappy.3585ef45.jpg"
+  },
+  {
+    "revision": "fe60fbe99b7caf5268d2698827a41db0",
+    "url": "/hoje-eh-dia/static/media/bolsomad.fe60fbe9.jpg"
+  },
+  {
+    "revision": "4704e7934ccddc7bfd3fde47f7d4a9f7",
+    "url": "/hoje-eh-dia/static/media/bolsothink.4704e793.jpeg"
+  }
+]);
