32ed478f9009f9a67ba296932cdfabacea943f23
Victor Chastinet <victorchastinet@live.com>
2019-08-14 23:47:20 -0300

    finishing up style

--- src/App.js
+++ src/App.js
@@ -1,53 +1,96 @@
 import React, { useState } from 'react';
-import mad from './assets/bolsomad.jpg'
-import happy from './assets/bolsohappy.jpg'
-import think from './assets/bolsothink.jpeg'
+
+import {
+  Button,
+  Container,
+  Image,
+  Content,
+  Form,
+  Heading,
+} from 'react-bulma-components/full';
+
+import mad from './assets/bolsomad.jpg';
+import happy from './assets/bolsohappy.jpg';
+import think from './assets/bolsothink.jpeg';
 
 import './App.css';
 
 const CURRENT_DATE = new Date();
 
-const mood = {
-  Sim: happy,
-  'Não': mad,
+const textColor = {
+  color: '#f8ffff',
 }
 
+const mood = {
+  Sim: happy,
+  Não: mad,
+  think: think
+};
+
 function App() {
-  const [answer, setAnswer] = useState('');
+  const [answer, setAnswer] = useState('...');
   const [lastDate, setLastDate] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = (event) => {
-    const [year, month, day] = lastDate.split('-');
-    const brDate = new Date(year, month - 1, day);
-
-    const deadLine = CURRENT_DATE - brDate;
-
     event.preventDefault();
-    const answer = deadLine >= 24 * Math.pow(10, 7) ? 'Sim' : 'Não';
-    setAnswer(answer);
-  }
+    setLoading(true);
+    setAnswer('think');
 
-  const handleChange = ({ target: { value }}) => {
+    setTimeout(() => {
+      const [year, month, day] = lastDate.split('-');
+      const brDate = new Date(year, month - 1, day);
+  
+      const deadLine = CURRENT_DATE - brDate;
+  
+      const answer = deadLine >= 24 * Math.pow(10, 7) ? 'Sim' : 'Não';
+      setAnswer(answer);
+      setLoading(false);
+    }, 1000);
+
+  };
+
+  const handleChange = ({ target: { value } }) => {
     setLastDate(value);
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <Container>
+      <Content>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="lastDate"> Qual foi a ultima vez que você fez cocô? </label>
-          <br />
-          <input value={lastDate} id="lastDate" onChange={handleChange} type="date" />
-          <br />
-          <img height="400" width="400" src={(!answer ? think : mood[answer])} alt="mad biro liro" />
-          <br />
-          <button type="submit">Hoje é dia de fazer cocô?</button>
+          <Form.Field>
+            <Form.Label style={textColor} size="large">
+              Qual foi a ultima vez que você fez cocô?
+            </Form.Label>
+            <Form.Control size="large" htmlFor="lastDate">
+              <Form.Input
+                size="large"
+                value={lastDate}
+                id="lastDate"
+                onChange={handleChange}
+                type="date"
+              />
+            </Form.Control>
+          </Form.Field>
+          <div className="image">
+            <Image
+              alt="bolso icon"
+              src={answer === '...' ? think : mood[answer]}
+              />
+          </div>
+          <Heading size={1} style={textColor}>{loading ? '...' : answer}</Heading>
+          <Button
+            loading={loading}
+            rounded
+            size="large"
+            color="danger"
+            type="submit"
+          >
+            Hoje é dia de fazer cocô?
+          </Button>
         </form>
-        <p>
-          {answer}
-        </p>
-      </header>
-    </div>
+      </Content>
+    </Container>
   );
 }
 
